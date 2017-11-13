6d6badee4f37ca79e49ea2b4a625951e44efa1e2
Richard Myatt <lydgate77@hotmail.co.uk>
2017-11-13 13:53:27 +0000

    working draft

--- js/quotes.js
+++ js/quotes.js
@@ -2,13 +2,42 @@
    Date: 17 October 2017
 */
 
+var quotations;
+var blq = document.querySelector('blockquote');
+var btn = document.querySelector('button');
+var cite = document.querySelector('cite');
+btn.addEventListener('click', changeTxt);
 
-// function to reveal and hide an element using the w3.css classes
-function reveal_hide(elem) {
-  var el =document.getElementById(elem);
-  if(el.className.indexOf("w3-show") == -1) {
-    el.className += " w3-show";
-  } else {
-    el.className = el.className.replace(" w3-show", "");
-  }
+var requestURL = 'json/quotations.json';
+var request = new XMLHttpRequest();
+
+function makeRequest() {
+  request.onreadystatechange = getJson;
+  request.open('GET', requestURL);
+  request.response = 'json';
+  request.send();
+
+  getJson();
 }
+
+function getJson() {
+  if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        quotations = JSON.parse(request.response);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+}
+
+function rnd() {
+  return Math.floor(Math.random() * 50);
+}
+
+function changeTxt() {
+  var r = rnd();
+  blq.innerText = quotations[r].quote;
+  cite.innerText = quotations[r].attribution;
+}
+
+window.onload = makeRequest();
